62dc15670fe93cd5828374555f369df5d2e8f1d8
Almas Khan <almas.rbm@gmail.com>
2022-05-28 02:25:47 +0600

    footer updated

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner';
 import Parts from './Parts';
 import Reviews from './Reviews';
@@ -11,6 +12,7 @@ const Home = () => {
             <Parts />
             <Summery />
             <Reviews />
+            <Footer />
         </div>
     );
 };
